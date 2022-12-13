a633cfdee627ee37d951c36ed55db2ecb57c0865
MinSeon <s_2___@naver.com>
2022-12-13 10:21:37 +0900

    feat: Todo 추가 기능 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Todo from "./components/Todo";
+import AddTodo from "./components/AddTodo";
 
 const App = () => {
   const [todoItems, setTodoItems] = useState([
@@ -11,9 +12,19 @@ const App = () => {
     { id: 2, title: "My todo2", done: false },
     { id: 3, title: "My todo3", done: true },
   ]);
+  const todoId = useRef(4);
+
+  const addItem = (newItem) => {
+    newItem.id = todoId.current++; // key를 위한 id 설정
+    newItem.done = false; // done 초기화
+
+    // 기존 todoItems를 유지하고, 새로운 newItem을 추가
+    setTodoItems([...todoItems, newItem]);
+  };
 
   return (
     <div className="App">
+      <AddTodo addItem={addItem} />
       {todoItems.map((item) => {
         return <Todo key={item.id} todo={item} />;
       })}
---
+++ client/src/components/AddTodo.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const AddTodo = ({ addItem }) => {
+  const [todoItem, setTodoItem] = useState({
+    title: "",
+  });
+
+  const onButtonClick = () => {
+    // props로 받아온 addItem 함수 실행
+    addItem(todoItem);
+    setTodoItem({
+      title: "",
+    });
+  };
+
+  const onEnterKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onButtonClick();
+    }
+  };
+
+  return (
+    <div className="AddTodo">
+      <input
+        type="text"
+        placeholder="Add your new Todo"
+        onChange={(e) => setTodoItem({ title: e.target.value })}
+        value={todoItem.title}
+        onKeyPress={onEnterKeyPress}
+      />
+      <button onClick={onButtonClick}>ADD</button>
+    </div>
+  );
+};
+
+export default AddTodo;
