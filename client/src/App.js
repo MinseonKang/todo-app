6bd1ea1656efc08a5516a7ba7ecbe096cc20cab2
MinSeon <s_2___@naver.com>
2022-12-13 12:01:18 +0900

    feat: Todo 삭제 기능 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,47 @@
 import { useState, useRef } from "react";
-import Todo from "./components/Todo";
 import AddTodo from "./components/AddTodo";
+import Todo from "./components/Todo";
 
 const App = () => {
   const [todoItems, setTodoItems] = useState([
     {
       id: 1,
-      title: "My todo1",
+      title: "My Todo1",
       done: false,
     },
-    { id: 2, title: "My todo2", done: false },
-    { id: 3, title: "My todo3", done: true },
+    {
+      id: 2,
+      title: "My Todo2",
+      done: false,
+    },
+    {
+      id: 3,
+      title: "My Todo3",
+      done: true,
+    },
   ]);
   const todoId = useRef(4);
 
   const addItem = (newItem) => {
+    // newItem - {id: xx, title: xx, done: false}
     newItem.id = todoId.current++; // key를 위한 id 설정
     newItem.done = false; // done 초기화
-
     // 기존 todoItems를 유지하고, 새로운 newItem을 추가
-    setTodoItems([...todoItems, newItem]);
+    setTodoItems([...todoItems, newItem]); // setTodoItems(todoItems.concat(newItem))
+  };
+
+  const deleteItem = (targetItem) => {
+    console.log(targetItem);
+    let newTodoItems = todoItems.filter((item) => item.id !== targetItem.id);
+    setTodoItems(newTodoItems);
   };
 
   return (
     <div className="App">
       <AddTodo addItem={addItem} />
       {todoItems.map((item) => {
-        return <Todo key={item.id} todo={item} />;
+        // console.log(item); // {id: 1, title: 'My Todo1', done: false}
+        return <Todo key={item.id} item={item} deleteItem={deleteItem} />;
       })}
     </div>
   );
--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,15 +1,25 @@
-const Todo = ({ todo }) => {
-  console.log(todo);
+import { useState } from "react";
+
+const Todo = ({ item, deleteItem }) => {
+  // console.log(item); // { id: 1, title: 'todo1', done: false, }
+  const { id, title, done } = item;
+  const [todoItem, setTodoItem] = useState(item);
+
+  const onDeleteBtnClick = () => {
+    deleteItem(todoItem);
+  };
+
   return (
     <div className="Todo">
       <input
         type="checkbox"
-        name="todo"
-        id={todo.id}
-        value="todo"
-        defaultChecked={todo.done}
+        id={`todo${id}`}
+        name={`todo${id}`}
+        value={`todo${id}`}
+        defaultChecked={done}
       />
-      <label htmlFor={todo.id}>{todo.title}</label>
+      <label htmlFor={`todo${id}`}>{title}</label>
+      <button onClick={onDeleteBtnClick}>DELETE</button>
     </div>
   );
 };
